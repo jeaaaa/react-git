6aa4c45a53f8135d401c17796fd86aa8bd0aef09
nolure <nolure@vip.qq.com>
2018-03-09 18:25:33 +0800

    lo

--- first_react_app/firstreact/src/index.js
+++ first_react_app/firstreact/src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ClickCounter from './ClickCounter.js';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<ClickCounter />, document.getElementById('root'));
 registerServiceWorker();
+
